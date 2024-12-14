3908ddedcd3811cc825b0b2f2bab4891ea70e488
livanjimenez <jimenez.livan14@gmail.com>
2024-12-13 21:50:19 -0500

    implementing node-cache instead of hashmap

--- cache.js
+++ cache.js
@@ -1,6 +1,26 @@
 const cache = new Map();
 const ttlMap = new Map();
 
+// TODO: Watch youtube video on node-cache
+// TODO: Redefine objectives for caching system
+
+const nodeCache = require('node-cache');
+const myCache = new nodeCache();
+
+const obj = { my: 'Special', variable: 42 };
+const obj2 = { my: 'other special', variable: 1337 };
+
+const success = myCache.mset([
+  { key: 'myKey', val: obj },
+  { key: 'myOtherKey', val: obj2 },
+]);
+
+if (!success) {
+  console.error('Failed to set cache');
+} else {
+  console.log('Cache set');
+}
+
 function getCache(key) {
   if (ttlMap.has(key) && ttlMap.get(key) < Date.now()) {
     cache.delete(key);
@@ -15,5 +35,10 @@ function setCache(key, value, ttl) {
   ttlMap.set(key, Date.now() + ttl);
 }
 
+function clearCache() {
+  cache.clear();
+  ttlMap.clear();
+}
+
 module.exports = cache;
-module.exports = { getCache, setCache };
+module.exports = { getCache, setCache, clearCache };
